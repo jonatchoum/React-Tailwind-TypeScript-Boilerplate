6232142a792361fc41785b97240eb8aac76ce1f8
jonatchoum <jona.atsu@gmail.com>
2023-01-24 13:25:52 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen grid place-content-center  gap-10 text-center">
+      <h1 className="text-xl underline">BoilerPlate</h1>
+      <div>React Tailwind TypeScript</div>
+    </div>
+  );
+}
+
+export default App;
